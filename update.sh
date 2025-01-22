ed40931b5c26c082be279c52655786f500328402
NunoHoid <nunoalexmart@gmail.com>
2025-01-22 15:51:30 +0000

    Add files via upload

--- update.sh
+++ update.sh
@@ -8,3 +8,8 @@ paccache -ruk0
 sudo pacman -Rns $(pacman -Qdtq)
 
 sudo DIFFPROG=colordiff pacdiff
+
+if [ -x "$(command -v flatpak)" ]; then
+	flatpak update -y
+	flatpak remove --delete-data --unused -y
+fi
