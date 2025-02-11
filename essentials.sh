562505baeddcdde978d2ec66b6a19d009312b326
NunoHoid <nunoalexmart@gmail.com>
2025-02-11 22:17:42 +0000

    Add files via upload

--- essentials.sh
+++ essentials.sh
@@ -1,7 +1,8 @@
-sudo pacman -S --needed \
+sudo pacman -S \
 bash-completion git man-db nano ntfs-3g tlp \
 colordiff pacman-contrib reflector \
 lib32-mesa lib32-vulkan-radeon mesa vulkan-radeon \
-noto-fonts noto-fonts-cjk ttf-martian-mono-nerd \
-firefox fragments libreoffice mpv steam zed \
-clang go gopls pyright ruff zig zls
+noto-fonts noto-fonts-cjk noto-fonts-emoji \
+gwenview haruna ktorrent okular plasma-meta \
+firefox ghostty libreoffice-fresh steam zed \
+clang go gopls pytright ruff zig zls
