aab3677ae58c227ddabecb165ea3125a003b096a
NunoHoid <nunoalexmart@gmail.com>
2025-02-12 13:22:02 +0000

    Add files via upload

--- essentials.sh
+++ essentials.sh
@@ -3,6 +3,7 @@ bash-completion git man-db nano ntfs-3g tlp \
 colordiff pacman-contrib reflector \
 lib32-mesa lib32-vulkan-radeon mesa vulkan-radeon \
 noto-fonts noto-fonts-cjk noto-fonts-emoji \
-ark dolphin gwenview haruna ktorrent okular plasma-meta \
+kwalletmanager plasma-meta partitionmanager \
+ark dolphin gwenview haruna ktorrent okular \
 firefox ghostty libreoffice-fresh steam zed \
 clang go gopls pyright ruff zig zls
