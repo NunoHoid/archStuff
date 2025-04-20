6a84afe74f0a7c6bbf7b35fa9456442e2cb8cd6c
NunoHoid <nunoalexmart@gmail.com>
2025-04-20 16:59:57 +0100

    Add files via upload

--- essentials.sh
+++ essentials.sh
@@ -1,7 +1,7 @@
 base="bash-completion dosfstools git man-db nano ntfs-3g pacman-contrib power-profiles-daemon reflector unrar"
 core="lib32-mesa lib32-vulkan-radeon noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra"
 kde="ark dolphin gwenview haruna isoimagewriter konsole kwalletmanager okular partitionmanager plasma-meta"
-extra="clang firefox go gopls libreoffice-fresh pyright qbittorrent ruff steam zed zig zls"
+extra="clang firefox go gopls libreoffice-fresh pyright qbittorrent ruff steam ttf-jetbrains-mono zed zig zls"
 
 sudo pacman -S --needed $base $core $kde $extra
 sudo pacman -D --asexplicit $base $core $kde $extra
